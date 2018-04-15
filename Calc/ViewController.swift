d21381fd0f67a361f36f89e49d62c8f55eeb9880
juan hurtado <juanhurtado@mbp-de-juan.mshome.net>
2018-04-15 18:26:55 -0500

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by juan hurtado on 15/04/18.
+//  Copyright © 2018 Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
